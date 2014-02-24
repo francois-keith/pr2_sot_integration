f58f14fb98ec3ed20e1ada46dedb785a18e96fa4
Francois Keith <keith@lirmm.fr>
2014-02-24 19:43:45 +0100

    FBFeedback: the free flyer should not be taken into account.

--- ctrl_interface/src/fb_bridge.cpp
+++ ctrl_interface/src/fb_bridge.cpp
@@ -17,64 +17,58 @@ FbBridge::FbBridge()
 //HARDCODED!!!!! PR2 ONLY - REFACTOR ME
 void FbBridge::loadConfig()
 {
-//   m_sot_order.insert(std::make_pair());
-  m_sot_order["base_joint_0"] = 0;
-  m_sot_order["base_joint_1"] = 1;
-  m_sot_order["base_joint_2"] = 2;
-  m_sot_order["base_joint_3"] = 3;
-  m_sot_order["base_joint_4"] = 4;
-  m_sot_order["base_joint_5"] = 5;
-  m_sot_order["bl_caster_rotation_joint"] = 6;
-  m_sot_order["bl_caster_l_wheel_joint"] = 7;
-  m_sot_order["bl_caster_r_wheel_joint"] = 8;
-  m_sot_order["br_caster_rotation_joint"] = 9;
-  m_sot_order["br_caster_l_wheel_joint"] = 10;
-  m_sot_order["br_caster_r_wheel_joint"] = 11;
-  m_sot_order["fl_caster_rotation_joint"] = 12;
-  m_sot_order["fl_caster_l_wheel_joint"] = 13;
-  m_sot_order["fl_caster_r_wheel_joint"] = 14;
-  m_sot_order["fr_caster_rotation_joint"] = 15;
-  m_sot_order["fr_caster_l_wheel_joint"] = 16;
-  m_sot_order["fr_caster_r_wheel_joint"] = 17;
-  m_sot_order["torso_lift_joint"] = 18;
-  m_sot_order["head_pan_joint"] = 19;
-  m_sot_order["head_tilt_joint"] = 20;
-  m_sot_order["l_shoulder_pan_joint"] = 21;
-  m_sot_order["l_shoulder_lift_joint"] = 22;
-  m_sot_order["l_upper_arm_roll_joint"] = 23;
-  m_sot_order["l_elbow_flex_joint"] = 24;
-  m_sot_order["l_forearm_roll_joint"] = 25;
-  m_sot_order["l_wrist_flex_joint"] = 26;
-  m_sot_order["l_wrist_roll_joint"] = 27;
-  m_sot_order["l_gripper_l_finger_joint"] = 28;
-  m_sot_order["l_gripper_l_finger_tip_joint"] = 29;
-  m_sot_order["l_gripper_motor_slider_joint"] = 30;
-  m_sot_order["l_gripper_motor_screw_joint"] = 31;
-  m_sot_order["l_gripper_r_finger_joint"] = 32;
-  m_sot_order["l_gripper_r_finger_tip_joint"] = 33;
-  m_sot_order["l_gripper_joint"] = 34;
-  m_sot_order["laser_tilt_mount_joint"] = 35;
-  m_sot_order["r_shoulder_pan_joint"] = 36;
-  m_sot_order["r_shoulder_lift_joint"] = 37;
-  m_sot_order["r_upper_arm_roll_joint"] = 38;
-  m_sot_order["r_elbow_flex_joint"] = 39;
-  m_sot_order["r_forearm_roll_joint"] = 40;
-  m_sot_order["r_wrist_flex_joint"] = 41;
-  m_sot_order["r_wrist_roll_joint"] = 42;
-  m_sot_order["r_gripper_l_finger_joint"] = 43;
-  m_sot_order["r_gripper_l_finger_tip_joint"] = 44;
-  m_sot_order["r_gripper_motor_slider_joint"] = 45;
-  m_sot_order["r_gripper_motor_screw_joint"] = 46;
-  m_sot_order["r_gripper_r_finger_joint"] = 47;
-  m_sot_order["r_gripper_r_finger_tip_joint"] = 48;
-  m_sot_order["r_gripper_joint"] = 49;
-  m_sot_order["torso_lift_motor_screw_joint"] = 50;
+  m_sot_order['bl_caster_rotation_joint'] = 0;
+  m_sot_order['bl_caster_l_wheel_joint'] = 1;
+  m_sot_order['bl_caster_r_wheel_joint'] = 2;
+  m_sot_order['br_caster_rotation_joint'] = 3;
+  m_sot_order['br_caster_l_wheel_joint'] = 4;
+  m_sot_order['br_caster_r_wheel_joint'] = 5;
+  m_sot_order['fl_caster_rotation_joint'] = 6;
+  m_sot_order['fl_caster_l_wheel_joint'] = 7;
+  m_sot_order['fl_caster_r_wheel_joint'] = 8;
+  m_sot_order['fr_caster_rotation_joint'] = 9;
+  m_sot_order['fr_caster_l_wheel_joint'] = 10;
+  m_sot_order['fr_caster_r_wheel_joint'] = 11;
+  m_sot_order['torso_lift_joint'] = 12;
+  m_sot_order['head_pan_joint'] = 13;
+  m_sot_order['head_tilt_joint'] = 14;
+  m_sot_order['l_shoulder_pan_joint'] = 15;
+  m_sot_order['l_shoulder_lift_joint'] = 16;
+  m_sot_order['l_upper_arm_roll_joint'] = 17;
+  m_sot_order['l_elbow_flex_joint'] = 18;
+  m_sot_order['l_forearm_roll_joint'] = 19;
+  m_sot_order['l_wrist_flex_joint'] = 20;
+  m_sot_order['l_wrist_roll_joint'] = 21;
+  m_sot_order['l_gripper_l_finger_joint'] = 22;
+  m_sot_order['l_gripper_l_finger_tip_joint'] = 23;
+  m_sot_order['l_gripper_motor_slider_joint'] = 24;
+  m_sot_order['l_gripper_motor_screw_joint'] = 25;
+  m_sot_order['l_gripper_r_finger_joint'] = 26;
+  m_sot_order['l_gripper_r_finger_tip_joint'] = 27;
+  m_sot_order['l_gripper_joint'] = 28;
+  m_sot_order['laser_tilt_mount_joint'] = 29;
+  m_sot_order['r_shoulder_pan_joint'] = 30;
+  m_sot_order['r_shoulder_lift_joint'] = 31;
+  m_sot_order['r_upper_arm_roll_joint'] = 32;
+  m_sot_order['r_elbow_flex_joint'] = 33;
+  m_sot_order['r_forearm_roll_joint'] = 34;
+  m_sot_order['r_wrist_flex_joint'] = 35;
+  m_sot_order['r_wrist_roll_joint'] = 36;
+  m_sot_order['r_gripper_l_finger_joint'] = 37;
+  m_sot_order['r_gripper_l_finger_tip_joint'] = 38;
+  m_sot_order['r_gripper_motor_slider_joint'] = 39;
+  m_sot_order['r_gripper_motor_screw_joint'] = 40;
+  m_sot_order['r_gripper_r_finger_joint'] = 41;
+  m_sot_order['r_gripper_r_finger_tip_joint'] = 42;
+  m_sot_order['r_gripper_joint'] = 43;
+  m_sot_order['torso_lift_motor_screw_joint'] = 44;
 }
 
 void FbBridge::feedbackCB(const sensor_msgs::JointStateConstPtr& msg)
 {
-  m_data.data.resize(m_sot_order.size());
-  m_data.data.assign(m_data.data.size(),0.0);
+  unsigned datasize = m_sot_order.size();
+  m_data.data.resize(datasize);
+  m_data.data.assign(datasize,0.0);
   for(unsigned i=0;i<msg->position.size();i++)
   {
     std::map<std::string,int>::iterator it = m_sot_order.find(msg->name[i]);
@@ -82,7 +76,6 @@ void FbBridge::feedbackCB(const sensor_msgs::JointStateConstPtr& msg)
     {
       m_data.data[it->second] = msg->position[i];
     }
-      
   }
   
   m_sot_fb.publish(m_data);
